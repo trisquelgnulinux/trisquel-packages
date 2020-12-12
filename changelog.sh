d395512e773f02063924e30a2c31027957e5d148
Ruben Rodriguez <ruben@gnu.org>
2020-12-12 16:30:27 +0100

    Updated changelog.sh

--- changelog.sh
+++ changelog.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-export DEBEMAIL=trisquel-devel@listas.trisquel.info
-export DEBFULLNAME="Trisquel GNU/Linux developers"
-
-CURRENTVERSION=$(head -n1 debian/changelog|sed 's/.*(//g; s/).*//g'|awk -F '-' '{print $1}')
-CURRENTSUBVERSION=$(head -n1 debian/changelog|sed 's/.*(//g; s/).*//g'|awk -F '-' '{print $2}')
-VERSION=$CURRENTVERSION-$(expr $CURRENTSUBVERSION + 1)
-
-echo | dch -v $VERSION  "$*"
+dch -M --force-distribution  $*
+dch -M --force-distribution -r ""
 
